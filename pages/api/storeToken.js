75ab8ffc4fa787b70b33bf9476899e2d3c33e613
RaphaPaixOnfly <raphael.paixao@onfly.com.br>
2024-05-24 12:04:43 -0300

    um

--- pages/api/storeToken.js
+++ pages/api/storeToken.js
@@ -1,11 +1,14 @@
 import corsMiddleware from '../../middleware/corsMiddleware';
 
+let storedToken = null; // Variável global para armazenar o token
+
 export default async function handler(req, res) {
   await corsMiddleware(req, res); // Aplica o middleware CORS
 
   if (req.method === 'POST') {
     const { token } = req.body;
     if (token) {
+      storedToken = token; // Armazena o token na variável global
       localStorage.setItem('auth-token', token); // Armazena o token no localStorage
       console.log('Token stored successfully:', token);
       res.status(200).json({ message: 'Token stored successfully' });
@@ -13,7 +16,7 @@ export default async function handler(req, res) {
       res.status(400).json({ error: 'Token not provided' });
     }
   } else if (req.method === 'GET') {
-    const token = localStorage.getItem('auth-token');
+    const token = storedToken || localStorage.getItem('auth-token'); // Recupera o token da variável global ou do localStorage
     console.log('Retrieving stored token:', token);
     if (token) {
       res.status(200).json({ token });
