55464c57e2c3886dba8f5203afd8f88522a09fcf
RaphaPaixOnfly <raphael.paixao@onfly.com.br>
2024-05-27 14:24:52 -0300

    reuniao3

--- components/ChartComponent.js
+++ components/ChartComponent.js
@@ -1,27 +1,57 @@
-import { Doughnut } from 'react-chartjs-2';
-
-const ChartComponent = ({ type, data, labels }) => {
-  const chartData = {
-    labels: labels,
-    datasets: [
-      {
-        label: 'Desempenho',
-        data: data,
-        backgroundColor: ['#4caf50', '#ddd'],
-      },
-    ],
-  };
-
-  const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-  };
-
-  return (
-    <div className="chart-container">
-      <Doughnut data={chartData} options={options} />
-    </div>
+import {
+    Chart as ChartJS,
+    ArcElement,
+    Tooltip,
+    Legend,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+  } from 'chart.js';
+  import { Doughnut, Bar } from 'react-chartjs-2';
+  
+  // Registrando elementos necessários
+  ChartJS.register(
+    ArcElement,
+    Tooltip,
+    Legend,
+    CategoryScale,
+    LinearScale,
+    BarElement
   );
-};
-
-export default ChartComponent;
+  
+  const ChartComponent = ({ type, data, labels }) => {
+    const chartData = {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Desempenho',
+          data: data,
+          backgroundColor: ['#4caf50', '#ddd'],
+        },
+      ],
+    };
+  
+    const options = {
+      responsive: true,
+      maintainAspectRatio: false,
+    };
+  
+    if (type === 'doughnut') {
+      return (
+        <div className="chart-container">
+          <Doughnut data={chartData} options={options} />
+        </div>
+      );
+    } else if (type === 'bar') {
+      return (
+        <div className="chart-container">
+          <Bar data={chartData} options={options} />
+        </div>
+      );
+    } else {
+      return null;
+    }
+  };
+  
+  export default ChartComponent;
+  
