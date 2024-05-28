f164b14542d0b367f2ce4379c230e78fb0e37719
RaphaPaixOnfly <raphael.paixao@onfly.com.br>
2024-05-28 09:50:50 -0300

    tentando arrumar layout

--- components/HorizontalBar.js
+++ components/HorizontalBar.js
@@ -1,16 +1,25 @@
 import styles from '../styles/Dashboard.module.css';
 
 const HorizontalBar = ({ percentage }) => {
-  const getColor = (value) => {
-    if (value === 100) return '#ADD8E6'; // Azul claro
-    if (value > 69) return '#4caf50'; // Verde
-    if (value >= 40 && value <= 69) return '#FFD700'; // Amarelo
-    return '#FF0000'; // Vermelho
+  const getBackgroundColor = (value) => {
+    if (value >= 70) return '#4caf50'; // Green
+    if (value >= 40 && value < 70) return '#ffeb3b'; // Yellow
+    if (value < 40) return '#f44336'; // Red
+    return '#2196f3'; // Light Blue for 100%
   };
 
   return (
-    <div className={styles.barContainer}>
-      <div className={styles.bar} style={{ width: `${percentage}%`, backgroundColor: getColor(percentage) }}></div>
+    <div className={styles['dashboard-bar-container']}>
+      <div
+        className={styles['dashboard-bar']}
+        style={{
+          width: `${percentage}%`,
+          backgroundColor: getBackgroundColor(percentage),
+        }}
+      ></div>
+      <div className={styles['dashboard-percentage-label']}>
+        {percentage}
+      </div>
     </div>
   );
 };
