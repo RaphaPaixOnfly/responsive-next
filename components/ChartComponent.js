c9d39bb793ada96298979b330f2ba6c80aef063f
RaphaPaixOnfly <raphael.paixao@onfly.com.br>
2024-05-28 09:21:55 -0300

    consertando layout

--- components/ChartComponent.js
+++ components/ChartComponent.js
@@ -7,24 +7,25 @@ import {
     BarElement,
   } from 'chart.js';
   import { Doughnut, Bar } from 'react-chartjs-2';
+  import styles from '../styles/Dashboard.module.css';
   
   ChartJS.register(ArcElement, Tooltip, CategoryScale, LinearScale, BarElement);
   
-  const getBackgroundColor = (value) => {
-    if (value >= 70) return '#4caf50'; // Green
-    if (value >= 40 && value < 70) return '#ffeb3b'; // Yellow
-    if (value < 40) return '#f44336'; // Red
-    return '#2196f3'; // Light Blue for 100%
-  };
-  
   const ChartComponent = ({ type, data, labels }) => {
+    const getColor = (value) => {
+      if (value === 100) return '#ADD8E6'; // Azul claro
+      if (value > 69) return '#4caf50'; // Verde
+      if (value >= 40 && value <= 69) return '#FFD700'; // Amarelo
+      return '#FF0000'; // Vermelho
+    };
+  
     const chartData = {
       labels: labels,
       datasets: [
         {
           label: 'Desempenho',
           data: data,
-          backgroundColor: [getBackgroundColor(data[0]), '#ddd'],
+          backgroundColor: [getColor(data[0]), '#ddd'],
           borderWidth: 1,
         },
       ],
@@ -33,6 +34,8 @@ import {
     const options = {
       responsive: true,
       maintainAspectRatio: false,
+      rotation: -90,
+      circumference: 180,
       plugins: {
         legend: {
           display: false,
@@ -42,13 +45,15 @@ import {
   
     if (type === 'doughnut') {
       return (
-        <div className="chart-container">
+        <div className={styles.chartContainer}>
+          <h2 className={styles.title}>Geral</h2>
           <Doughnut data={chartData} options={options} />
+          <div className={styles.percentageLabel}>{data[0]}</div>
         </div>
       );
     } else if (type === 'bar') {
       return (
-        <div className="chart-container">
+        <div className={styles.chartContainer}>
           <Bar data={chartData} options={options} />
         </div>
       );
