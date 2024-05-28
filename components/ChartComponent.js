3d7c95c0727c0fe9dd5a6f786e57536c21c135f6
RaphaPaixOnfly <raphael.paixao@onfly.com.br>
2024-05-28 08:53:54 -0300

    28/05

--- components/ChartComponent.js
+++ components/ChartComponent.js
@@ -12,13 +12,20 @@ import {
   ChartJS.register(ArcElement, Tooltip, CategoryScale, LinearScale, BarElement);
   
   const ChartComponent = ({ type, data, labels }) => {
+    const getColor = (value) => {
+      if (value === 100) return '#ADD8E6'; // Azul claro
+      if (value > 69) return '#4caf50'; // Verde
+      if (value >= 40 && value <= 69) return '#FFD700'; // Amarelo
+      return '#FF0000'; // Vermelho
+    };
+  
     const chartData = {
       labels: labels,
       datasets: [
         {
           label: 'Desempenho',
           data: data,
-          backgroundColor: ['#4caf50', '#ddd'],
+          backgroundColor: [getColor(data[0]), '#ddd'],
           borderWidth: 1,
         },
       ],
@@ -27,15 +34,11 @@ import {
     const options = {
       responsive: true,
       maintainAspectRatio: false,
+      rotation: -90,
+      circumference: 180,
       plugins: {
         legend: {
           display: false,
-        },
-      },
-      layout: {
-        padding: {
-          top: 20,
-          bottom: 20,
         },
       },
     };
@@ -43,9 +46,9 @@ import {
     if (type === 'doughnut') {
       return (
         <div className={styles.chartContainer}>
-          <h2>Geral</h2>
+          <h2 className={styles.title}>Geral</h2>
           <Doughnut data={chartData} options={options} />
-          <div className={styles.percentageLabel}>{data[0]}%</div>
+          <div className={styles.percentageLabel}>{data[0]}</div>
         </div>
       );
     } else if (type === 'bar') {
--- components/HorizontalBar.js
+++ components/HorizontalBar.js
@@ -1,12 +1,16 @@
-import styles from '../styles/HorizontalBar.module.css';
+import styles from '../styles/Dashboard.module.css';
 
 const HorizontalBar = ({ percentage }) => {
+  const getColor = (value) => {
+    if (value === 100) return '#ADD8E6'; // Azul claro
+    if (value > 69) return '#4caf50'; // Verde
+    if (value >= 40 && value <= 69) return '#FFD700'; // Amarelo
+    return '#FF0000'; // Vermelho
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.barContainer}>
-        <div className={styles.bar} style={{ width: `${percentage}%` }}></div>
-      </div>
-      <div className={styles.label}>{percentage}%</div>
+    <div className={styles.barContainer}>
+      <div className={styles.bar} style={{ width: `${percentage}%`, backgroundColor: getColor(percentage) }}></div>
     </div>
   );
 };
