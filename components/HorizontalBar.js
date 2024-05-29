fc041e30e8f70b514f9b0dd3a74ea33fcec43fa9
RaphaPaixOnfly <raphael.paixao@onfly.com.br>
2024-05-29 15:24:49 -0300

    atualizando cor dos valores

--- components/HorizontalBar.js
+++ components/HorizontalBar.js
@@ -1,24 +1,22 @@
 import styles from '../styles/Dashboard.module.css';
 
-const HorizontalBar = ({ percentage }) => {
-  const getBackgroundColor = (value) => {
-    if (value >= 70) return '#4caf50'; // Green
-    if (value >= 40 && value < 70) return '#ffeb3b'; // Yellow
-    if (value < 40) return '#f44336'; // Red
-    return '#2196f3'; // Light Blue for 100%
-  };
+const getBackgroundColor = (value) => {
+  if (value >= 70) return '#4caf50'; // Green
+  if (value >= 40 && value < 70) return '#ffeb3b'; // Yellow
+  if (value < 40) return '#f44336'; // Red
+  return '#2196f3'; // Light Blue for 100%
+};
 
+const HorizontalBar = ({ percentage }) => {
+  const backgroundColor = getBackgroundColor(percentage);
   return (
-    <div className={styles['dashboard-bar-container']}>
+    <div className={styles['bar-container']}>
       <div
-        className={styles['dashboard-bar']}
-        style={{
-          width: `${percentage}%`,
-          backgroundColor: getBackgroundColor(percentage),
-        }}
-      ></div>
-      <div className={styles['dashboard-percentage-label']}>
-        {percentage}
+        className={styles['bar']}
+        style={{ width: `${percentage}%`, backgroundColor }}
+      />
+      <div className={styles['percentage-label']} style={{ color: backgroundColor }}>
+        {percentage}%
       </div>
     </div>
   );
