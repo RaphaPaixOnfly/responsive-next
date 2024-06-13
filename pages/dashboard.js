1cf6167c4a8fa9ae020644f8bf5ed4d147773e30
RaphaPaixOnfly <raphael.paixao@onfly.com.br>
2024-06-13 13:43:21 -0300

    dashboard posicionado

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -33,58 +33,56 @@ export default function Dashboard() {
 
   return (
     <div className={styles['dashboard-container']}>
-      <div className={styles['dashboard-row']}>
-        <div className={styles['dashboard-column1']}>
-          <ChartComponent
-            type="doughnut"
-            data={[data.geral, 100 - data.geral]}
-            labels={['Desempenho Geral', '']}
-          />
-          <div className={styles['geral']}>{data.geral}</div>
-        </div>
-        <div className={styles['dashboard-column2']}>
-          <div className={styles['dashboard-text-container']}>
-            <h2>Desempenho Geral</h2>
-            <p>{data.respostaGeral}</p>
+      <div className={styles['dashboard-ranking']}>
+        <div className={styles['dashboard-row']}>
+          <div className={styles['dashboard-column1']}>
+            <div className={styles['chart-container']}>
+              <ChartComponent
+                type="doughnut"
+                data={[data.geral, 100 - data.geral]}
+                labels={['Desempenho Geral', '']}
+              />
+              <div className={styles['geral']}>{data.geral}</div>
+            </div>
+            <div className={styles['dashboard-text-container']}>
+              <h2>Desempenho Geral</h2>
+              
+            </div>
           </div>
         </div>
       </div>
-      <div className={styles['dashboard-row2']}>
+      <div className={styles['dashboard-fundamentos']}>
+        <div className={styles['dashboard-row2']}>
           <div className={styles['dashboard-column']}>
             <HorizontalBar percentage={data.notaOperacional} nota={data.operacional} topico="Operacional" />
-            
             <p>{data.respostaOperacional}</p>
           </div>
           <div className={styles['dashboard-column']}>
             <HorizontalBar percentage={data.notaTecauto} nota={data.tecauto} topico="Tecnologia e Automação" />
-            
             <p>{data.respostaTecauto}</p>
           </div>
         </div>
         <div className={styles['dashboard-row2']}>
           <div className={styles['dashboard-column']}>
             <HorizontalBar percentage={data.notaControlecustos} nota={data.controlecustos} topico="Controle de Custos" />
-            
             <p>{data.respostaCusto}</p>
           </div>
           <div className={styles['dashboard-column']}>
             <HorizontalBar percentage={data.notaPlanejamento} nota={data.planejamento} topico="Planejamento" />
-            
             <p>{data.respostaPlanejamento}</p>
           </div>
         </div>
         <div className={styles['dashboard-row2']}>
           <div className={styles['dashboard-column']}>
             <HorizontalBar percentage={data.notaSatisfacaodoviajante} nota={data.satisfacaodoviajante} topico="Satisfação do Viajante" />
-            
             <p>{data.respostaSatisfacao}</p>
           </div>
           <div className={styles['dashboard-column']}>
             <HorizontalBar percentage={data.notaCompliance} nota={data.compliance} topico="Compliance e Políticas da Empresa" />
-            
             <p>{data.respostaCompliance}</p>
           </div>
         </div>
       </div>
+    </div>
   );
 }
